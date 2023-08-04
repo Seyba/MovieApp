74f888c7690008c0cc6ec71fdb0a047475460f8c
Mahamadou <mmohmo@yahoo.com>
2023-08-04 10:42:07 -0400

    environment variable added

--- src/App.js
+++ src/App.js
@@ -3,15 +3,23 @@ import { movieList } from "./utils.js/movieList";
 import MovieDisplay from "./components/MovieDisplay";
 import Form from "./components/Form";
 
+
 export default function App() {
-  const apiKey = "98e3fb1f"
+  const apiKey = process.env.REACT_APP_MOVIE_API_KEY
+
   const [movie, setMovie] = useState(null)
   
   useEffect(() => {
-    const fetchMovie = async (movieList) =>{
-      const movieIndx =  Math.floor(Math.random() * movieList.length);
-      const randomMovie = movieList[movieIndx]
-      console.log(randomMovie)
+    
+    const fetchMovie = async (searchTerm) =>{
+      //* turn the search term into an array 
+      const searchArr = searchTerm.split('')
+
+      //* getting a random letter from the search array
+      const idx =  Math.floor(Math.random() * searchArr.length);
+      const randomMovie = searchArr[idx]
+
+      //* a random letter is picked on every refresh of the page
       try{
         const movie = await fetch(
           `http://www.omdbapi.com/?apikey=${apiKey}&t=${randomMovie}`
@@ -29,7 +37,7 @@ export default function App() {
   
   const getMovie = async searchTerm => {
     try{
-      //* display random movie if no search term provided
+      //* display random movie if no search term is provided
       const movieIndx =  Math.floor(Math.random() * movieList.length);
       const randomMovie = movieList[movieIndx]
 
